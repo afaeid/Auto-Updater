6b96af7c4425227b2b53fbb6ac34382f55c33e76
MUHAMMAD AFAEID SARKAR <muhammadafaeidsarkar@gmail.com>
2026-06-11 10:44:29 +0000

    solved a  minor bug

--- actionPresenter.js
+++ actionPresenter.js
@@ -10,7 +10,7 @@ const presentAction = (actionHist, config)=>{
       console.log(chalk.greenBright(`Created ${actionHist.created}`))
       console.log(chalk.yellowBright(`Updated ${actionHist.updated}`))
       console.log(chalk.redBright(`Deleted ${actionHist.deleted}`))
-      console.log(chalk.bgGreenBright.white.bold(`Total ${actionHist.created + actionHist.updated + actionHist.deleted} in ${((Date.now() - process.startTime)/1000).toFixed(2)} second(s)`))
+      console.log(chalk.bgGreenBright.bold(`Total ${actionHist.created + actionHist.updated + actionHist.deleted} in ${((Date.now() - process.startTime)/1000).toFixed(2)} second(s)`))
       console.log(chalk.gray("In"), chalk.yellowBright.underline(config.executingDir))
       console.log(chalk.gray("at", new Date().toString()))
       
